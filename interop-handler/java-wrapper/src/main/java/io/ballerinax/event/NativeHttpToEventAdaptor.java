541fe81aad21ec1775a339b2c991a60f3586197e
Hasitha Athukorala <hasithaisuru@gmail.com>
2021-10-06 14:04:23 +0530

    Modify native java adaptor

--- interop-handler/java-wrapper/src/main/java/io/ballerinax/event/NativeHttpToEventAdaptor.java
+++ interop-handler/java-wrapper/src/main/java/io/ballerinax/event/NativeHttpToEventAdaptor.java
@@ -17,30 +17,23 @@
 package io.ballerinax.event;
 
 import io.ballerina.runtime.api.Environment;
-import io.ballerina.runtime.api.Future;
-import io.ballerina.runtime.api.Module;
 import io.ballerina.runtime.api.async.Callback;
 import io.ballerina.runtime.api.async.StrandMetadata;
 import io.ballerina.runtime.api.creators.ErrorCreator;
-import io.ballerina.runtime.api.creators.ValueCreator;
-import io.ballerina.runtime.api.types.MethodType;
-import io.ballerina.runtime.api.utils.StringUtils;
-import io.ballerina.runtime.api.values.BArray;
 import io.ballerina.runtime.api.values.BError;
 import io.ballerina.runtime.api.values.BMap;
 import io.ballerina.runtime.api.values.BObject;
 import io.ballerina.runtime.api.values.BString;
 
-import java.util.ArrayList;
 import static io.ballerina.runtime.api.utils.StringUtils.fromString;
 
 public class NativeHttpToEventAdaptor {
     public static Object invokeRemoteFunction(Environment env, BObject adaptor, BMap<BString, Object> message, BString eventName, BString eventFunction, BObject serviceObj) {
-        Future balFuture = env.markAsync();
-        Module module = ModuleUtils.getModule();
-        StrandMetadata metadata = new StrandMetadata(module.getOrg(), module.getName(), module.getVersion(),
+        var balFuture = env.markAsync();
+        var module = ModuleUtils.getModule();
+        var metadata = new StrandMetadata(module.getOrg(), module.getName(), module.getVersion(),
                 eventName.getValue());
-        Object[] args = new Object[]{message, true};
+        var args = new Object[]{message, true};
         env.getRuntime().invokeMethodAsync(serviceObj, eventFunction.getValue(), null, metadata, new Callback() {
             @Override
             public void notifySuccess(Object result) {
