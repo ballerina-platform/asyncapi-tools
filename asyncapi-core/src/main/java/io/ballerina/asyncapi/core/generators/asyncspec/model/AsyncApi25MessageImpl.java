470b699127550cc41e427acd043a211e8607f262
thushalya <weeeratharava@g,ail.com>
2023-04-06 19:03:49 +0530

    Add class document to AsyncApi25MessageImpl

--- asyncapi-core/src/main/java/io/ballerina/asyncapi/core/generators/asyncspec/model/AsyncApi25MessageImpl.java
+++ asyncapi-core/src/main/java/io/ballerina/asyncapi/core/generators/asyncspec/model/AsyncApi25MessageImpl.java
@@ -1,10 +1,19 @@
 package io.ballerina.asyncapi.core.generators.asyncspec.model;
 
-import io.apicurio.datamodels.models.asyncapi.v25.AsyncApi25Message;
-
+/**
+ * In Apicurio data model they are using JsonNodes for bindings
+ * therefore we have to convert Java object to a Jackson ObjectNode we have to use new ObjectMapper()
+ * <p>
+ * This {@code AsyncApi25MessageImpl} contains details related to AsyncApi25MessageImpl. It has overrided the original
+ * Apicurio isEntity method and set it into false, then it will not be printed. Reason because this is in the original
+ * one entity attribute has become true by default and it is also printing in the asyncapi definition when using
+ * jackson's objectmapper
+ *
+ * @since 2.0.0
+ */
 public class AsyncApi25MessageImpl extends io.apicurio.datamodels.models.asyncapi.v25.AsyncApi25MessageImpl {
     @Override
-    public boolean isEntity(){
+    public boolean isEntity() {
         return false;
     }
 
