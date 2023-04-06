d1ad24d156ae8d006447124251a044ff5d5e10ec
thushalya <weeeratharava@g,ail.com>
2023-04-06 08:27:30 +0530

    Create AsyncApi25MessageImpl class by overriding original AsyncApi25MessageImpl of apicurio model

---
+++ asyncapi-core/src/main/java/io/ballerina/asyncapi/core/generators/asyncspec/model/AsyncApi25MessageImpl.java
@@ -0,0 +1,11 @@
+package io.ballerina.asyncapi.core.generators.asyncspec.model;
+
+import io.apicurio.datamodels.models.asyncapi.v25.AsyncApi25Message;
+
+public class AsyncApi25MessageImpl extends io.apicurio.datamodels.models.asyncapi.v25.AsyncApi25MessageImpl {
+    @Override
+    public boolean isEntity(){
+        return false;
+    }
+
+}
