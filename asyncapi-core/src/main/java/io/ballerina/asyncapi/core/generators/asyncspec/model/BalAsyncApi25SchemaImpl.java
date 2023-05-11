389df0e1aa549d7d18369f412efe3671b6020f16
thushalya <weeeratharava@gmail.com>
2023-05-11 07:38:26 +0530

    Add JsonDeserialize for schemaSchemaListUnion and BooleanSchemaUnion

--- asyncapi-core/src/main/java/io/ballerina/asyncapi/core/generators/asyncspec/model/BalAsyncApi25SchemaImpl.java
+++ asyncapi-core/src/main/java/io/ballerina/asyncapi/core/generators/asyncspec/model/BalAsyncApi25SchemaImpl.java
@@ -2,6 +2,24 @@ package io.ballerina.asyncapi.core.generators.asyncspec.model;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.apicurio.datamodels.models.Schema;
+import io.apicurio.datamodels.models.asyncapi.v25.AsyncApi25SchemaImpl;
+import io.apicurio.datamodels.models.union.BooleanSchemaUnion;
+import io.apicurio.datamodels.models.union.BooleanUnionValueImpl;
+import io.apicurio.datamodels.models.union.SchemaListUnionValueImpl;
+import io.apicurio.datamodels.models.union.SchemaSchemaListUnion;
+
+@JsonDeserialize(as = BalAsyncApi25SchemaImpl.class)
+interface MockSchema extends Schema {
+
+
+}
+
+//@JsonTypeInfo( use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "class")
+//@JsonSubTypes({
+//        @JsonSubTypes.Type(value = AsyncApi25SchemaImpl.class)
+//})
 
 /**
  * In Apicurio data model they are using JsonNodes for bindings
@@ -16,16 +34,28 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 public class BalAsyncApi25SchemaImpl extends io.apicurio.datamodels.models.asyncapi.v25.AsyncApi25SchemaImpl {
 
+//    public BalAsyncApi25SchemaImpl() {
+//        super();
+//    }
+
+    @JsonDeserialize(as = BalAsyncApi25SchemaImpl.class)
+    private SchemaSchemaListUnion items;
+
+    @JsonDeserialize(as = BalAsyncApi25SchemaImpl.class)
+    private BooleanSchemaUnion additionalProperties;
+
     @JsonIgnore
     @Override
     public boolean isEntity() {
         return true;
     }
 
+
     @JsonIgnore
     @Override
     public boolean isSchema() {
         return true;
     }
-}
 
+
+}
